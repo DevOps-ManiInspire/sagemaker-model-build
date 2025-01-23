be2a4dc9ff91e8d7362fbfe416e2d094ab1a39a4
mani1263 <manikandaninspire@gmail.com>
2025-01-23 13:08:29 +0530

    { test

--- enhancement.sh
+++ enhancement.sh
@@ -11,6 +11,6 @@ export PYTHONPATH="$HOME/.local/${SAGEMAKER_PROJECT_NAME_ID}:$PYTHONPATH"
 #   --kwargs "{\"region\":\"${AWS_REGION}\",\"role\":\"${SAGEMAKER_PIPELINE_ROLE_ARN}\",\"default_bucket\":\"${ARTIFACT_BUCKET}\",\"pipeline_name\":\"${SAGEMAKER_PROJECT_NAME_ID}\",\"model_package_group_name\":\"${SAGEMAKER_PROJECT_NAME_ID}\",\"base_job_prefix\":\"${SAGEMAKER_PROJECT_NAME_ID}\"}"
 echo "%%%%%%"
 
-echo "Community : $community"
+echo "Community : ${community}"
 echo "AWS Region selected is : $@"
 echo "%%%%%%"
